8ba38f986b37b6e7a3a747db27fa5885183706b9
Kein.jiang <Kein.jiang@tuya.com>
2022-03-29 13:05:21 +0800

    longest_substring_without_repeating_characters@v2

--- src/problems/longest_substring_without_repeating_characters.rs
+++ src/problems/longest_substring_without_repeating_characters.rs
@@ -1,57 +1,19 @@
-use std::collections::HashMap;
-
 // 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
 // s 由英文字母、数字、符号和空格组成
+
+// 滑动窗口问题
 pub fn longest_substring_without_repeating_characters(s: String) -> i32 {
-    let s: Vec<char> = s.chars().collect();
-    let len = s.len();
-    if len <= 1 {
-        return len as i32;
-    }
+    let mut l = 0;
+    let mut cache = vec![0; 129];
+    let mut ret = 0;
 
-    let mut substring_length: usize = 1;
+    s.chars().enumerate().for_each(|(idx, char)| {
+        l = l.max(cache[char as usize]);
+        cache[char as usize] = idx + 1;
+        ret = ret.max(idx - l + 1);
+    });
 
-    let mut i = 0;
-    let mut j = 0;
-    let mut cache: HashMap<char, usize> = HashMap::new();
-
-    while i < len {
-        let first_char = s[i];
-        cache.insert(first_char, i);
-
-        // 不可能出现更长的子序列了
-        if len - i <= substring_length {
-            break;
-        }
-
-        if j <= i {
-            j = i + 1;
-        }
-
-        while j < len {
-            let cur_char = s[j];
-            let last_pos = cache.get(&cur_char);
-
-            match last_pos {
-                Some(&pos) => {
-                    for idx in i..pos + 1 {
-                        cache.remove(&s[idx]);
-                    }
-                    i = pos;
-                    break;
-                }
-                None => {
-                    substring_length = substring_length.max(j - i + 1);
-                    cache.insert(cur_char, j);
-                    j += 1;
-                }
-            }
-        }
-
-        i += 1;
-    }
-
-    substring_length as i32
+    ret as i32
 }
 
 #[test]
