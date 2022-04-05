372acedadb7a7302a631015e4e9685beffd2666e
plimeor <mervinjin@gmail.com>
2022-04-05 18:01:13 +0800

    chore: add ListNode::from

--- src/common/list_node.rs
+++ src/common/list_node.rs
@@ -1,17 +1,45 @@
 // Definition for singly-linked list.
 
 #[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+pub struct ListNode<T: Clone = i32> {
+    pub val: T,
+    pub next: Option<Box<Self>>,
 }
 
-impl ListNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+impl<T: Clone> ListNode<T> {
+    #[inline]
+    pub fn new(val: T) -> Self {
+        ListNode { next: None, val }
     }
-  }
+
+    pub fn from(list: Vec<T>) -> Option<Box<Self>> {
+        if list.len() == 0 {
+            return None;
+        }
+
+        let mut head: Option<Box<Self>> = None;
+        let mut tail = &mut head;
+
+        for i in 0..list.len() {
+            let val = &list[i];
+            *tail = Some(Box::new(Self::new(val.clone())));
+            tail = &mut tail.as_mut().unwrap().next;
+        }
+
+        head
+    }
+}
+
+#[test]
+fn test() {
+    let list = ListNode {
+        val: 1,
+        next: Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode::new(3))),
+        })),
+    };
+
+    assert_eq!(Box::new(list), ListNode::from(vec![1, 2, 3]).unwrap());
+
 }
