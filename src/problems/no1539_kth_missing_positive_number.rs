cafe2c24c3f3bb63103852a9a72cb08dc94ac8d8
Kein.jiang <Kein.jiang@tuya.com>
2022-04-06 12:20:22 +0800

    feat: kth-missing-positive-number@v2

--- src/problems/no1539_kth_missing_positive_number.rs
+++ src/problems/no1539_kth_missing_positive_number.rs
@@ -6,29 +6,30 @@ impl Solution {
         let mut l = 0;
         let mut r = arr.len() - 1;
 
-        // 对条件 key < k 进行右边界查找
+        if arr[l] > k {
+            return k;
+        }
+
+        // 对条件 missed_count < k 进行右边界查找
         while l < r {
             let mid = (l + r + 1) / 2;
-            let key = arr[mid] - (mid + 1) as i32;
-            if key < k {
+            // arr[mid] 所缺失对整数数量
+            let missed_count = arr[mid] - (mid + 1) as i32;
+
+            // 由于上面判断过 arr[0] <= k
+            // 所以第一个 if 语句一定会被执行
+            // （如果没执行，就需要在 while 循环后面打补丁了）
+            if missed_count < k {
                 l = mid;
             } else {
                 r = mid - 1;
             }
         }
 
-        // arr[l] 缺失的整数数量
+        // arr[l] 所缺失的整数数量
         let missed_count = arr[l] - (l + 1) as i32;
 
-        if missed_count < k {
-            // 目标位于右侧
-            // 之所以加 k，而不是 k - 1，是为了排除 arr[l]
-            arr[l] - missed_count + k
-        } else {
-            // 目标位于左侧
-            // 之所以加 k - 1，而不是 k，是为了包含 arr[l] - missed_count
-            arr[l] - missed_count + (k - 1)
-        }
+        k - missed_count + arr[l]
     }
 }
 
